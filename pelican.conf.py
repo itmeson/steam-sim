c737d1f807746b5e3b3dbcdf7c52081da33b8d63
Mark Betnel <itmeson@gmail.com>
2013-01-25 11:29:24 -0500

    first setup

---
+++ pelican.conf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Mark Betnel"
+SITENAME = u"STEAM - SIMS"
+SITEURL = 'http://markbetnel.com/steam-sim'
+
+TIMEZONE = 'America/New_York'
+GOOGLE_ANALYTICS = "UA-20141547-1"
+DEFAULT_LANG='en'
+
+# Blogroll
+LINKS =  (
+    ('Wolfram Alpha', 'http://www.wolframalpha.com/'),
+    ('Khan Academy', 'http://www.khanacademy.org'),
+    ('Math Fun Facts', 'http://www.math.hmc.edu/funfacts/'),
+    ('Google Finance', 'http://google.com/finance'),
+    ('R - Statistics', 'http://cran.r-project.org'),
+    ('DESMOS', 'http://www.desmos.com'),
+    ('Rosalind', 'http://rosalind.info'),
+    ('Project Euler', 'http://projecteuler.net')	
+        )
+
+# Social widget
+SOCIAL = (
+         ('ProblemsFeed', SITEURL + '/feeds/problems.atom.xml'),
+         ('LessonsFeed', SITEURL + '/feeds/lessons.atom.xml'),
+	 ('QuizzesFeed', SITEURL + '/feeds/quizzes.atom.xml')
+	 )
+
+DEFAULT_PAGINATION = 7
+DISPLAY_PAGES_ON_MENU = False
+
+    
---
+++ src/overview/overview.rst
@@ -0,0 +1,17 @@
+Overview
+########
+
+:date: 2013-01-25
+:summary: What's this all about
+:category: overview
+:tags:
+
+
+=====
+About
+=====
+
+This is a project for students to learn about science, technology, engineering, art, and math (steam)  through solving  challenging puzzles that are designed to require specific skills ... blah blah blah.
+
+(Lorem ipsum dolor)
+
