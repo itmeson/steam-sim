297d53ea2ad5b915948a6ae991070bbc89e97c47
Wil Hall <wil@wilhall.com>
2013-01-26 19:10:38 -0500

    Updated static files and added labels & badges
    Added QTip2
    Added FontAwesome
    Minified CSS & JS
    Added custom styles for Bootstrap labels & badges for use referring to the S.T.E.A.M components visually

--- res/steam.sql
+++ res/steam.sql
@@ -1,20 +1,26 @@
 USE steam;
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE users (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	username VARCHAR(12) NOT NULL UNIQUE,
-	email VARCHAR(60) NOT NULL UNIQUE,
-	password VARCHAR(60) NOT NULL,
-	first_name VARCHAR(12) NOT NULL,
-	last_name VARCHAR(12) NOT NULL
-	admin INT NOT NULL DEFAULT 0
-);
+| uas_users_accounts | CREATE TABLE `uas_users_accounts` (
+  `User_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `School_ID` int(11) NOT NULL,
+  `Username` varchar(25) NOT NULL,
+  `Email` varchar(60) NOT NULL,
+  `Password` varchar(60) NOT NULL,
+  `FirstName` varchar(25) NOT NULL,
+  `LastName` varchar(25) NOT NULL,
+  PRIMARY KEY (`User_ID`),
+  UNIQUE KEY `Username` (`Username`),
+  UNIQUE KEY `Email` (`Email`),
+  UNIQUE KEY `Password` (`Password`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 |
 
 DROP TABLE IF EXISTS `sessions`;
-CREATE TABLE sessions (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	user_id INT NOT NULL UNIQUE,
-	token VARCHAR(12) NOT NULL UNIQUE,
-	lastupdate TIMESTAMP DEFAULT NOW()
+| uas_users_sessions | CREATE TABLE `uas_users_sessions` (
+  `Session_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `User_ID` int(11) NOT NULL,
+  `Token` varchar(12) NOT NULL,
+  `LastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Session_ID`),
+  UNIQUE KEY `Token` (`Token`)
 );
